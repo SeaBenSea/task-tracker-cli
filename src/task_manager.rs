940f2a68ca0b6604b9e76af94abe0b20c41e906e
Deniz Günenç <52115910+SeaBenSea@users.noreply.github.com>
2024-11-09 01:00:28 +0300

    Switch to HashMap (#3)

--- src/task_manager.rs
+++ src/task_manager.rs
@@ -2,6 +2,7 @@ use crate::models::{Status, Task};
 use chrono::Local;
 use serde_json::json;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{Read, Write},
     path::Path,
@@ -12,10 +13,10 @@ const FILE_PATH: &str = "tasks.json";
 pub struct TaskManager;
 
 impl TaskManager {
-    fn load_tasks() -> Vec<Task> {
+    fn load_tasks() -> HashMap<u32, Task> {
         if !Path::new(FILE_PATH).exists() {
             File::create(FILE_PATH).expect("Failed to create JSON file.");
-            return Vec::new();
+            return HashMap::new();
         }
 
         let mut file = File::open(FILE_PATH).expect("Failed to open JSON file.");
@@ -24,17 +25,19 @@ impl TaskManager {
             .expect("Failed to read data.");
 
         if data.is_empty() {
-            return Vec::new();
+            return HashMap::new();
         } else {
-            return serde_json::from_str(&data).expect("Failed to parse JSON.");
+            let tasks: Vec<Task> = serde_json::from_str(&data).expect("Failed to parse JSON.");
+            let tasks_map = tasks.into_iter().map(|task| (task.id, task)).collect();
+            return tasks_map;
         }
     }
 
-    fn save_tasks(tasks: &[Task]) {
-        let mut tasks = tasks.to_vec();
-        tasks.sort_by(|a, b| a.id.cmp(&b.id));
+    fn save_tasks(tasks: &HashMap<u32, Task>) {
+        let mut tasks_vec: Vec<&Task> = tasks.values().collect();
+        tasks_vec.sort_by(|a, b| a.id.cmp(&b.id));
 
-        let data = json!(tasks);
+        let data = json!(tasks_vec);
         let mut file = File::create(FILE_PATH).expect("Failed to create JSON file.");
         file.write_all(data.to_string().as_bytes())
             .expect("Failed to write data.");
@@ -42,11 +45,7 @@ impl TaskManager {
 
     pub fn add_task(description: String) {
         let mut tasks = Self::load_tasks();
-        let id = if tasks.is_empty() {
-            1
-        } else {
-            tasks.last().unwrap().id + 1
-        };
+        let id = tasks.keys().max().cloned().unwrap_or(0) + 1;
 
         let task = Task {
             id,
@@ -56,7 +55,7 @@ impl TaskManager {
             updated_at: Local::now(),
         };
 
-        tasks.push(task);
+        tasks.insert(id, task);
         println!("Task added successfully with ID: {}", id);
 
         Self::save_tasks(&tasks);
@@ -65,9 +64,7 @@ impl TaskManager {
 
     pub fn update_task(id: u32, description: String) {
         let mut tasks = Self::load_tasks();
-        let task = tasks.iter_mut().find(|task| task.id == id);
-
-        match task {
+        match tasks.get_mut(&id) {
             Some(task) => {
                 task.description = description;
                 task.updated_at = Local::now();
@@ -84,33 +81,29 @@ impl TaskManager {
 
     pub fn delete_task(id: u32) {
         let mut tasks = Self::load_tasks();
-        let task = tasks.iter().position(|task| task.id == id);
+        if tasks.remove(&id).is_some() {
+            println!("Task with ID {} deleted successfully.", id);
 
-        match task {
-            Some(index) => {
-                tasks.remove(index);
-                println!("Task with ID {} deleted successfully.", id);
-
-                Self::save_tasks(&tasks);
-                Self::list_tasks(None);
-            }
-            None => {
-                eprintln!("Task with ID {} not found.", id);
-            }
+            Self::save_tasks(&tasks);
+            Self::list_tasks(None);
+        } else {
+            eprintln!("Task with ID {} not found.", id);
         }
     }
 
     pub fn mark_in_progress_task(id: u32) {
         let mut tasks = Self::load_tasks();
-        let task = tasks.iter_mut().find(|task| task.id == id);
-
-        match task {
+        match tasks.get_mut(&id) {
             Some(task) => {
-                task.status = Status::InProgress;
-                task.updated_at = Local::now();
-                println!("Task with ID {} is now in progress.", id);
+                if task.status == Status::InProgress {
+                    eprintln!("Task with ID {} is already in progress.", id);
+                } else {
+                    task.status = Status::InProgress;
+                    task.updated_at = Local::now();
+                    println!("Task with ID {} marked as in progress.", id);
 
-                Self::save_tasks(&tasks);
+                    Self::save_tasks(&tasks);
+                }
                 Self::list_tasks(None);
             }
             None => {
@@ -121,15 +114,18 @@ impl TaskManager {
 
     pub fn mark_done(id: u32) {
         let mut tasks = Self::load_tasks();
-        let task = tasks.iter_mut().find(|task| task.id == id);
-
-        match task {
+        match tasks.get_mut(&id) {
             Some(task) => {
-                task.status = Status::Done;
-                task.updated_at = Local::now();
-                println!("Task with ID {} is now done.", id);
+                if task.status == Status::Done {
+                    eprintln!("Task with ID {} is already done.", id);
+                } else {
+                    task.status = Status::Done;
+                    task.updated_at = Local::now();
+                    println!("Task with ID {} marked as done.", id);
 
-                Self::save_tasks(&tasks);
+                    Self::save_tasks(&tasks);
+                }
+
                 Self::list_tasks(None);
             }
             None => {
@@ -140,15 +136,17 @@ impl TaskManager {
 
     pub fn restart_task(id: u32) {
         let mut tasks = Self::load_tasks();
-        let task = tasks.iter_mut().find(|task| task.id == id);
-
-        match task {
+        match tasks.get_mut(&id) {
             Some(task) => {
-                task.status = Status::Todo;
-                task.updated_at = Local::now();
-                println!("Task with ID {} is now restarted.", id);
+                if task.status == Status::Todo {
+                    eprintln!("Task with ID {} is already in Todo status.", id);
+                } else {
+                    task.status = Status::Todo;
+                    task.updated_at = Local::now();
+                    println!("Task with ID {} restarted successfully.", id);
 
-                Self::save_tasks(&tasks);
+                    Self::save_tasks(&tasks);
+                }
                 Self::list_tasks(None);
             }
             None => {
@@ -160,36 +158,32 @@ impl TaskManager {
     pub fn list_tasks(filter: Option<String>) {
         let tasks = Self::load_tasks();
 
-        let filtered_tasks = match filter {
-            Some(filter) => match filter.as_str() {
-                "all" => tasks.clone(),
-                "done" => tasks
-                    .iter()
-                    .filter(|task| task.status == Status::Done)
-                    .cloned()
-                    .collect(),
-                "in-progress" => tasks
-                    .iter()
-                    .filter(|task| task.status == Status::InProgress)
-                    .cloned()
-                    .collect(),
-                "not-done" => tasks
-                    .iter()
-                    .filter(|task| task.status != Status::Done)
-                    .cloned()
-                    .collect(),
-                _ => {
-                    eprintln!("Usage: task-tracker-cli list [all | done | in-progress | not-done], default is all.");
-                    return;
-                }
-            },
-            None => tasks.clone(),
+        let mut filtered_tasks: Vec<&Task> = match filter.as_deref() {
+            Some("all") | None => tasks.values().collect(),
+            Some("done") => tasks
+                .values()
+                .filter(|task| task.status == Status::Done)
+                .collect(),
+            Some("in-progress") => tasks
+                .values()
+                .filter(|task| task.status == Status::InProgress)
+                .collect(),
+            Some("not-done") => tasks
+                .values()
+                .filter(|task| task.status != Status::Done)
+                .collect(),
+            _ => {
+                eprintln!("Usage: task-tracker-cli list [all | done | in-progress | not-done], default is all.");
+                return;
+            }
         };
 
-        if tasks.is_empty() {
+        if filtered_tasks.is_empty() {
             println!("No tasks found.");
             return;
         }
+
+        filtered_tasks.sort_by_key(|task| task.id);
 
         println!(
             "{: <5} | {: <20} | {: <10} | {: <20} | {: <20}",
